7d6fe3b41af0395fa775a791bee06423fc9ffc2a
Dan Ackerson <dan@ackerson.de>
2012-10-07 14:28:02 +0200

    success whoami and date cmds impl'd

--- homepage_module_controllers.rb
+++ homepage_module_controllers.rb
@@ -7,7 +7,19 @@ module Homepage::Controllers
     end
 
     def post
-      @result = {:now=>Time.now.utc.to_s}
+      @result = {:now=>Time.now.to_s}
+	    @headers['Content-Type'] = "application/json"
+			@result.to_json
+    end
+  end
+
+  class Whoami < R '/whoami'
+  	def post
+  		user_ip = @env['REMOTE_ADDR']
+  		user_agent = @env['HTTP_USER_AGENT']
+
+      @result = {:who=>"#{user_ip} (#{user_agent})"}
+
 	    @headers['Content-Type'] = "application/json"
 			@result.to_json
     end
