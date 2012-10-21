c66fb81a2b38ee0320620e757a981fc907d5d809
Dan Ackerson <dan@ackerson.de>
2012-10-21 12:03:22 +0200

    working directions in chrome and ff with initial empty destination set to Munich

--- public/js/offworld.js
+++ public/js/offworld.js
@@ -1,95 +1,96 @@
 function popupClose(id) {
-    $('#' + id).hide();
+  $('#' + id).hide();
 }
 
 (function($) {
-    var id = 1;
-    $( "#mvvPopup" ).draggable({ handle: "p.border" });
-    $( "#drivePopup" ).draggable({ handle: "p.border" });
-    $( "div, p.border" ).disableSelection();
+  var id = 1;
+  $( "#mvvPopup" ).draggable({ handle: "p.border" });
+  $( "#drivePopup" ).draggable({ handle: "p.border" });
+  //$( "div, p.border" ).disableSelection(); //what was this for? it disables textbox selection!
 
-    term = $('#term1').terminal(function(command, term) {
-        var commands = command.split(' ');
-        if (commands.length > 0) {
-            try {
-                switch (commands[0]) {
-                    case 'help':
-                        term.echo("[[gi;#00FF00;]`whoami`, `date`, `clear`, `drive [DESTINATION]`, `mvv`]");
-                        break;
+  term = $('#term1').terminal(function(command, term) {
+    var commands = command.split(' ');
+    if (commands.length > 0) {
+      try {
+        switch (commands[0]) {
+          case 'help':
+            term.echo("[[gi;#00FF00;]`whoami`, `date`, `clear`, `drive [DESTINATION]`, `mvv`]");
+            break;
 
-                    case 'date':
-                    case 'whoami':
-                        simpleAjaxCall(command, "query-param");
-                        break;
+          case 'date':
+          case 'whoami':
+            simpleAjaxCall(command, "query-param");
+            break;
 
-                    case 'drive':
-                        showPopup(commands[0]);
-                        if (commands[1]) {
-                            document.getElementById('address').value = commands[1];
-                            getDrivingDirections();
-                        } else {
-                            document.getElementById('address').value = '';
-                            emptyMap();
-                        }
-                        break;
-
-                    case 'mvv':
-                        showPopup(commands[0]);
-                        break;
-
-                    default:
-                        var result = window.eval(command);
-                        if (result !== undefined) {
-                            term.echo(new String(result));
-                        }
-                        break;
-                }
-            } catch(e) {
-                term.echo("[[guib;#FFFF00;]" + new String(e) + "] (try `help`)");
+          case 'drive':
+            showPopup(commands[0]);
+            
+            if (commands[1]) {
+              document.getElementById('address').value = commands[1];
+            } else {
+              document.getElementById('address').value = 'Munich';
             }
-        } else {
-           term.echo('');
+
+            getDrivingDirections();
+            break;
+
+          case 'mvv':
+            showPopup(commands[0]);
+            break;
+
+          default:
+            var result = window.eval(command);
+            if (result !== undefined) {
+                term.echo(new String(result));
+            }
+            break;
         }
-    }, {
-        greetings: 'Welcome Offworld (type `help` for available cmds)',
-        name: 'term1',
-        enabled: false,
-        prompt: 'dan@ackerson.de:~ $ '
-    });
-
-    function showPopup(id) {
-        $('#' + id + 'Popup').show();
+      } catch(e) {
+        term.echo("[[guib;#FFFF00;]" + new String(e) + "] (try `help`)");
+      }
+    } else {
+      term.echo('');
     }
+  }, {
+    greetings: 'Welcome Offworld (type `help` for available cmds)',
+    name: 'term1',
+    enabled: false,
+    prompt: 'dan@ackerson.de:~ $ '
+  });
 
-    /* simple ajax call where typed cmd string is SAME as remote URI AND data set */
-    function simpleAjaxCall(command, query_param) {
-        term.pause();
+  function showPopup(id) {
+    $('#' + id + 'Popup').fadeIn('slow');
+  }
 
-        //$.jrpc is helper function which creates json-rpc request
-        $.jrpc(command,                         // uri
-            id++,
-            query_param, 
-            [command],                          // command
-            function(data) {
-              term.resume();
-              if (data.error) {
-                term.error(data.error.message);
-              } else {
-                term.echo(data[command]);       // data set
-              }
-            },
-            function(xhr, status, error) {
-                term.error('[AJAX] ' + status + ' - Server reponse is: \n' + 
-                            xhr.responseText);
-                term.resume();
-            }
-        ); // rpc call
-    }
+  /* simple ajax call where typed cmd string is SAME as remote URI AND data set */
+  function simpleAjaxCall(command, query_param) {
+    term.pause();
 
-    term.mouseout(function() {
-        term.focus(false);
-    });
-    term.mouseover(function() {
-        term.focus(true);
-    });
+    //$.jrpc is helper function which creates json-rpc request
+    $.jrpc(command,                         // uri
+      id++,
+      query_param, 
+      [command],                          // command
+      function(data) {
+        term.resume();
+        if (data.error) {
+          term.error(data.error.message);
+        } else {
+          term.echo(data[command]);       // data set
+        }
+      },
+      function(xhr, status, error) {
+        term.error('[AJAX] ' + status + ' - Server reponse is: \n' + 
+                    xhr.responseText);
+        term.resume();
+      }
+    ); // rpc call
+  }
+
+  term.mouseout(function() {
+    term.focus(false);
+  });
+  term.mouseover(function() {
+    term.focus(true);
+  });
 })(jQuery);
--- public/js/offworld_google_maps.js
+++ public/js/offworld_google_maps.js
@@ -7,6 +7,111 @@ var homeMarker;
 var homeLocation;
 var directionsDisplay;
 var directionsService;
+var ff_first_load = true;
+
+function init_googlemaps() {
+  geocoder = new google.maps.Geocoder();
+  directionsDisplay = new google.maps.DirectionsRenderer();
+
+  green_marker = new google.maps.MarkerImage(
+    '/images/marker_greenA.png',
+    new google.maps.Size(32, 32),   // size
+    new google.maps.Point(0,0),     // origin
+    new google.maps.Point(16, 32)   // anchor
+  );
+}
+
+function getCurrentLocation() {
+  if (navigator.geolocation) { // Try HTML5 geolocation
+    navigator.geolocation.getCurrentPosition(function(position) {
+      currentLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+      homeLocation = geocoder.geocode({'latLng': currentLatLng}, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+          if (results[1]) {
+            homeLocation = results[1].formatted_address;
+            document.getElementById('home_location').innerHTML = 
+              "<p style='font-size:14px;margin-left:10px;margin-bottom:0px;'><img style='height:24px;width:16px;vertical-align:middle;' src='/images/marker_greenA.png'>&nbsp;&nbsp;&nbsp;<b>" + homeLocation + "</b></p><hr>";
+            return homeLocation;
+          }
+        }
+      });
+    }, function() {
+      handleNoGeolocation(true);
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleNoGeolocation(false);
+  }
+
+  return currentLatLng;
+}
+
+function getDrivingDirections() {
+  currentLatLng = getCurrentLocation();
+
+  var address = document.getElementById('address').value;
+  var mapOptions = {
+    zoom: 8,
+    mapTypeId: google.maps.MapTypeId.ROADMAP  // ROADMAP, SATELLITE, HYBRID, TERRAIN
+  };
+
+  map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+
+  geocoder.geocode( { 'address': address }, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      destinationLatLng = results[0].geometry.location; 
+      if ($.browser.mozilla && ff_first_load) {
+        alert('Driving to: ' + destinationLatLng);
+        ff_first_load = false;
+      }
+         
+      homeMarker = new google.maps.Marker({
+          map: map,
+          icon: green_marker,
+          position: currentLatLng
+      });
+      directionsDisplay.setMap(map); // clear any past results
+
+      var directionsRequest = {
+        origin: homeMarker.position,
+        destination: destinationLatLng,
+        travelMode: google.maps.TravelMode.DRIVING // DRIVING, WALKING, BICYCLING, TRANSIT
+      }
+      homeMarker.setVisible(false); // remove dupe marker as directionsDisplay above sets home marker too
+
+      directionsService = new google.maps.DirectionsService();
+      directionsService.route(directionsRequest, function(result, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+          directionsDisplay.setDirections(result);
+
+          var steps = result.routes[0].legs[0].steps
+          var result = "";
+          var totalTime = 0;
+          var totalDist = 0;
+
+          for ( var step in steps ) {
+            result += "<p style='margin-left:10px;margin-top:0px;'><b>" + (parseInt(step) + 1) + ".</b> <span style='font-size:14px;'>" + steps[step].instructions + " (<span style='font-style:italic;font-size:12px;color:blue;'>" + steps[step].distance.text + " - " + steps[step].duration.text + "</span>)</span></p>";
+
+            totalTime += steps[step].duration.value;
+            totalDist += steps[step].distance.value;
+          }
+
+          hours = parseInt( totalTime / 3600 ) % 24;
+          minutes = parseInt( totalTime / 60 ) % 60;
+          totalTime = hours + "hr " + (minutes < 10 ? "0" + minutes : minutes) + "mins";
+
+          document.getElementById('directions').innerHTML = result;
+          document.getElementById('home_location').innerHTML = 
+            "<p style='font-size:14px;margin-left:10px;margin-bottom:0px;'><img style='height:24px;width:16px;vertical-align:middle;' src='/images/marker_greenA.png'>&nbsp;&nbsp;&nbsp;<b>" + homeLocation + "</b></p>" + 
+            "<p style='font-size:14px;margin-left:10px;margin-bottom:0px;'><img style='height:24px;width:16px;vertical-align:middle;' src='/images/marker_greenB.png'>&nbsp;&nbsp;&nbsp;<b>" + document.getElementById('address').value + "</b> (<span style='font-size:14px;font-style:italic;font-weight:bold;color:blue;'>" + totalDist/1000 + " km - " + totalTime + "</span>)</p><hr>";
+        }
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
 
 function handleNoGeolocation(errorFlag) {
     if (errorFlag) {
@@ -25,131 +130,3 @@ function handleNoGeolocation(errorFlag) {
     map.setCenter(options.position);
 }
 
-// TODO DELETE ANY PREVIOUS DESTINATIONS FROM MAP! (in map_directions div?)
-function emptyMap() {
-    var mapOptions = {
-      zoom: 8,
-      mapTypeId: google.maps.MapTypeId.ROADMAP  // ROADMAP, SATELLITE, HYBRID, TERRAIN
-    };
-    
-    map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
-
-    green_marker = new google.maps.MarkerImage(
-        'http://maps.gstatic.com/mapfiles/markers2/marker_greenA.png',
-        new google.maps.Size(32, 32),   // size
-        new google.maps.Point(0,0),     // origin
-        new google.maps.Point(16, 32)   // anchor
-    );
-
-    homeMarker = new google.maps.Marker({
-        map: map,
-        icon: green_marker,
-        position: currentLatLng
-    });
-
-    map.setCenter(currentLatLng);
-    document.getElementById('home_location').innerHTML = 
-      "<p style='font-size:14px;margin-left:10px;margin-bottom:0px;'><img style='height:24px;width:16px;vertical-align:middle;' src='https://maps.gstatic.com/mapfiles/markers2/marker_greenA.png'>&nbsp;&nbsp;&nbsp;<b>" + homeLocation + "</b></p><hr>";
-    document.getElementById('directions').innerHTML = "";
-}
-
-function init_googlemaps() {
-    geocoder = new google.maps.Geocoder();
-
-    
-    // Try HTML5 geolocation
-    if(navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        var pos = new google.maps.LatLng(position.coords.latitude,
-                                         position.coords.longitude);
-
-        // be careful to store TRUE coordinates! 
-        // after adding markers and/or infoWindows the center of the map changes 
-        // and subsequent calls to get the location from .getCenter() will be WRONG!
-        currentLatLng = pos;
-        geocoder.geocode({'latLng': currentLatLng}, function(results, status) {
-              if (status == google.maps.GeocoderStatus.OK) {
-                if (results[1]) {
-                  homeLocation = results[1].formatted_address;
-                  document.getElementById('home_location').innerHTML = 
-                    "<p style='font-size:14px;margin-left:10px;margin-bottom:0px;'><img style='height:24px;width:16px;vertical-align:middle;' src='https://maps.gstatic.com/mapfiles/markers2/marker_greenA.png'>&nbsp;&nbsp;&nbsp;<b>" + homeLocation + "</b></p><hr>";
-                }
-              } 
-            });
-
-          green_marker = new google.maps.MarkerImage(
-            'http://maps.gstatic.com/mapfiles/markers2/marker_greenA.png',
-            new google.maps.Size(32, 32),   // size
-            new google.maps.Point(0,0),     // origin
-            new google.maps.Point(16, 32)   // anchor
-          );
-          homeMarker = new google.maps.Marker({
-            map: map,
-            icon: green_marker,
-            position: currentLatLng
-          });
-
-      }, function() {
-        handleNoGeolocation(true);
-      });
-    } else {
-      // Browser doesn't support Geolocation
-      handleNoGeolocation(false);
-    }
-}
-
-function getDrivingDirections() {
-    var address = document.getElementById('address').value;
-    if (!address) address = currentLatLng;
-
-    var mapOptions = {
-      zoom: 8,
-      mapTypeId: google.maps.MapTypeId.ROADMAP  // ROADMAP, SATELLITE, HYBRID, TERRAIN
-    };
-
-    map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
-    geocoder.geocode( { 'address': address }, function(results, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
-        destinationLatLng = results[0].geometry.location; 
-        
-        directionsDisplay = new google.maps.DirectionsRenderer();
-        directionsDisplay.setMap(map); // clear any past results
-
-        var directionsRequest = {
-          origin: homeMarker.position,
-          destination: destinationLatLng,
-          travelMode: google.maps.TravelMode.DRIVING // DRIVING, WALKING, BICYCLING, TRANSIT
-        }
-
-        directionsService = new google.maps.DirectionsService();
-        directionsService.route(directionsRequest, function(result, status) {
-          if (status == google.maps.DirectionsStatus.OK) {
-            directionsDisplay.setDirections(result);
-
-            var steps = result.routes[0].legs[0].steps
-            var result = "";
-            var totalTime = 0;
-            var totalDist = 0;
-
-            for ( var step in steps ) {
-              result += "<p style='margin-left:10px;margin-top:0px;'><b>" + (parseInt(step) + 1) + ".</b> <span style='font-size:14px;'>" + steps[step].instructions + " (<span style='font-style:italic;font-size:12px;color:blue;'>" + steps[step].distance.text + " - " + steps[step].duration.text + "</span>)</span></p>";
-
-              totalTime += steps[step].duration.value;
-              totalDist += steps[step].distance.value;
-            }
-
-            hours = parseInt( totalTime / 3600 ) % 24;
-            minutes = parseInt( totalTime / 60 ) % 60;
-            totalTime = hours + "hr " + (minutes < 10 ? "0" + minutes : minutes) + "mins";
-
-            document.getElementById('directions').innerHTML = result;
-            document.getElementById('home_location').innerHTML = 
-              "<p style='font-size:14px;margin-left:10px;margin-bottom:0px;'><img style='height:24px;width:16px;vertical-align:middle;' src='http://maps.gstatic.com/mapfiles/markers2/marker_greenA.png'>&nbsp;&nbsp;&nbsp;<b>" + homeLocation + "</b></p>" + 
-              "<p style='font-size:14px;margin-left:10px;margin-bottom:0px;'><img style='height:24px;width:16px;vertical-align:middle;' src='http://maps.gstatic.com/mapfiles/markers2/marker_greenB.png'>&nbsp;&nbsp;&nbsp;<b>" + document.getElementById('address').value + "</b> (<span style='font-size:14px;font-style:italic;font-weight:bold;color:blue;'>" + totalDist/1000 + " km - " + totalTime + "</span>)</p><hr>";
-          }
-        });
-      } else {
-        alert('Geocode was not successful for the following reason: ' + status);
-      }
-    });
-}
